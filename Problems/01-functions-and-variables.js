087a384e9fd1d6eec42a39c21fe06f64f0f6601b
lincolnchiu <lincolncth@gmail.com>
2019-10-03 15:34:47 +0800

    test.js issue

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -76,7 +76,8 @@ function returnArray (first, second, third) {
  * @returns {string} a sentence  composed from the first 3 elements of the array
  */
 function splitArray (thisArray) {
-  return thisArray[0] + " " + thisArray[1] " was a " + thisArray[2] + ".";
+  var a = thisArray[0] + " " + thisArray[1] " was a " + thisArray[2] + ".";
+  return 'hello';
 }
 
 // Problem 5
